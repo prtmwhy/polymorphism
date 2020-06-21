1f16ba661554abe85eb13ec2ceab2cb55a536b33
prtmwhy <61861806+prtmwhy@users.noreply.github.com>
2020-06-21 18:32:10 +0700

    polymorphism final

---
+++ KaryawanHarian.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLab8.ClassInduk;
+
+namespace TugasLab8.ClassAnak
+{
+    public class KaryawanHarian : Karyawan
+    {
+        public int JumlahJamKerja { get; set; }
+        public int UpahPerJam { get; set; }
+        
+        public override double Gaji() {
+            return JumlahJamKerja * UpahPerJam;
+        }
+    }
+}
---
+++ KaryawanTetap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLab8.ClassInduk;
+
+namespace TugasLab8.ClassAnak
+{
+    public class KaryawanTetap : Karyawan
+    {
+        public double GajiBulanan { get; set; }
+        
+        public override double Gaji() {
+            return GajiBulanan;
+        }
+    }
+}
